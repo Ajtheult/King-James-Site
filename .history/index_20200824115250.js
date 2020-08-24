b6d4d6602b59dcae5463fda688cc2328c1e88593
Ajtheult <asaphfranksboi@gmail.com>
2020-08-24 11:58:21 +0300

    Put the finishing touches on the task. Have to make mobile responsive

---
+++ .history/index_20200824115250.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
---
+++ .history/index_20200824115259.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "100px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
